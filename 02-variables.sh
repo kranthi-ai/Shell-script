cfcc52ed97254556495f5d6b7094e8cdfe824651
kranthi-ai <kranthi.k1810@gmail.com>
2024-12-31 20:16:58 +0530

    conversation added

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,4 +2,5 @@
 
 echo "Rahim:: Hi Robert"
 echo "Robert:: Helo Rahim"
-echo "Rahim::Im good. How are you?'
+echo "Rahim:: Im good. How are you?"
+echo "Robert:: im fine"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON1:: Helo $PERSON2"
+echo "$PERSON1:: Im good. How are you?"
+echo "$PERSON2:: im fine"

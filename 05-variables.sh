aceede00c8ad3c8b0cd2ebe244889fb5659d6624
kranthi-ai <kranthi.k1810@gmail.com>
2025-01-01 17:14:39 +0530

    shell script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,11 @@
-#I/bin/bash
+#!/bin/bash
 
 echo "Please enter your username::" # text entered here will be added as value to varaiable
 
 read -s USERNAME #here USERNAME is the variable name
 
 echo "username entered: $USERNAME"
+
+echo "Please enter your password::"
+
+read -s PASSWORD

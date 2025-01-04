3cf8770dbeb98975ffad58dc04149a032e551528
kranthi-ai <kranthi.k1810@gmail.com>
2025-01-04 09:36:57 +0530

    shell script

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-then
+ then
     echo "ERROR:: You must have sudo access to execute it"
     exit 1  #other than 0
 fi
@@ -11,17 +11,17 @@ fi
 dnf install mysql -y
 
 if [ $? -ne 0 ]
-then 
+ then 
    echo "Installing MySQL... FAILURE"
    exit 1
-else
+ else
    echo "Installing My SQL...SUCCESS"
  fi
 
  dnf install git -y
 
  if [ $? -ne 0 ]
- then
+  then
      echo "Installing Git..FAILURE"
      exit 1
  else

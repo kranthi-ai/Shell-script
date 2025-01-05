447cb282158761378e4d14428e19922fc3294cfc
kranthi-ai <kranthi.k1810@gmail.com>
2025-01-05 09:14:32 +0530

    shell script

---
+++ 13-logs.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\3[32m"
+Y="\e[33m"
+LOGS_FOLDER="/var/log/shell-script-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP.log"
+
+
+VALIDAATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2....$R FAILURE"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS"
+    fi
+}
+
+echo "script started executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+ then
+    echo "ERROR:: You must have sudo access to execute it"
+    exit 1  #other than 0
+fi
+
+dnf list installed mysql  &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ]
+then # not installed
+ dnf install mysql -y     &>>$LOG_FILE_NAME
+ VALIDATE $? "Installing MySQL"
+ 
+ else
+    echo -e "MYSQL is already installed...$Y INSTALLED"
+    fi
+
+
+
+ dnf list installed git  &>>$LOG_FILE_NAME
+
+  if [ $? -ne 0 ]
+then 
+      dnf install git -y &>>$LOG_FILE_NAME
+      VALIDATE $? "Installing Git"
+ else
+      echo -e "GIT is already installed...$Y INSTALLED" 
+ fi  
+
+    
